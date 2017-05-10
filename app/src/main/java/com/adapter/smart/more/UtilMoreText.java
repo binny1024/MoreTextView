85312dfe2a4568afca7f7d1a8a1cd9c0054ad987
xubinbin1024 <596928539@qq.com>
2017-05-10 12:46:53 +0800

    Initial commit

---
+++ app/src/main/java/com/adapter/smart/more/UtilMoreText.java
@@ -0,0 +1,125 @@
+package com.adapter.smart.more;
+
+
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by xbb on 2017/5/10.
+ */
+
+/*
+* 简单的富文本工具类
+*
+* 使用说明 new UtilMoreText().setClickableSpan(tv2,msg);
+* */
+public class UtilMoreText  {
+
+    private OnSpanTextClickListener mSpanTextClickListener;
+
+    private TextView mTextView;//显示富文本的控件
+    private String mOriMsg;//全部文本信息
+
+    private boolean spread =true;
+    private int mStartPos;
+    private int mEndPos;
+    private Integer mSpanTextColor;
+
+    /**
+     * @param textView  显示富文本的textView
+     * @param oriMsg    文本原始信息
+     * @param spanTextClickListener
+     */
+    public void setClickableSpan(TextView textView, String oriMsg,int spanTextColor,OnSpanTextClickListener spanTextClickListener){
+        mSpanTextClickListener = spanTextClickListener;
+        mSpanTextColor =spanTextColor;
+
+        mTextView = textView;
+        mOriMsg = oriMsg;
+        textView.setMovementMethod(LinkMovementMethod.getInstance());//必须设置否则无效
+        textView.setText(part());
+
+    }
+
+    /** 重载这个方法 可用于与其他view交互
+     * @param textView
+     * @param oriMsg
+     */
+    public void setClickableSpan(TextView textView, String oriMsg,int spanTextColor){
+        mTextView = textView;
+        mOriMsg = oriMsg;
+        mSpanTextColor =spanTextColor;
+        textView.setMovementMethod(LinkMovementMethod.getInstance());//必须设置否则无效
+        textView.setText(part());
+    }
+    public void setClickableSpan(TextView textView, String oriMsg, int spanTextColor, int startPos, int endPos){
+        mTextView = textView;
+        mOriMsg = oriMsg;
+        mStartPos = startPos;
+        mEndPos = endPos;
+        mSpanTextColor =spanTextColor;
+        textView.setMovementMethod(LinkMovementMethod.getInstance());//必须设置否则无效
+        textView.setText(part());
+    }
+
+    class SpanTextClickable extends ClickableSpan implements View.OnClickListener{
+        @Override
+        public void onClick(View widget) {
+            if (spread) {//调用展开的方法
+                spread = false;
+                if (mSpanTextClickListener == null) {
+                    mTextView.setText(open());
+                }else {
+                    mSpanTextClickListener.setSpanText(mTextView,open());
+                }
+
+            }else {
+                spread = true;
+                if (mSpanTextClickListener == null) {
+                    mTextView.setText(part());
+                }else {
+                    mSpanTextClickListener.setSpanText(mTextView,part());
+                }
+
+            }
+        }
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            if (mSpanTextColor == null) {
+                ds.setColor(Color.BLUE);
+            }else {
+                ds.setColor(ds.linkColor);
+            }
+
+            ds.setUnderlineText(false);    //去除超链接的下划线
+        }
+    }
+
+    private SpannableString part() {
+        String temp = mOriMsg.substring(0,mOriMsg.length()/2)+"...展开";
+        SpannableString spanableInfo = new SpannableString(temp);
+
+        spanableInfo.setSpan(new  SpanTextClickable(), mStartPos==0?temp.length()-2:mStartPos, mEndPos==0?temp.length():mEndPos,
+                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        return spanableInfo;
+    }
+
+    private SpannableString open() {
+        String temp = mOriMsg+"收起";
+        SpannableString spanableInfo = new SpannableString(temp);
+        spanableInfo.setSpan(new  SpanTextClickable(), mStartPos==0?temp.length()-2:mStartPos, mEndPos==0?temp.length():mEndPos,
+                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        return spanableInfo;
+    }
+    public interface OnSpanTextClickListener {
+        void setSpanText(TextView view,SpannableString s);
+    }
+}
